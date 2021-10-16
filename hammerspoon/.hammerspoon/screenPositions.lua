4a895384e80424d01537491195b9f0e41b71b62b
Prahalad Ramji <prahaladramji@users.noreply.github.com>
2021-10-16 23:05:17 +1100

    osx specific

---
+++ hammerspoon/.hammerspoon/screenPositions.lua
@@ -0,0 +1,10 @@
+---------------------
+-- Screen positioning
+---------------------
+
+function cycleScreens()
+    local window = hs.window.focusedWindow()
+    local currentScreen = window:screen()
+
+    window:moveToScreen(currentScreen:next(), ensureInScreenBounds)
+end
