2332953240b8a6f48bad328323342e68ef4f318c
Prahalad Ramji <740299+prahaladramji@users.noreply.github.com>
2025-02-25 21:58:16 +1100

    configure clipboard sharing over tmux and vim

--- dot_config/nvim/lua/core/autocommands.lua
+++ dot_config/nvim/lua/core/autocommands.lua
@@ -19,3 +19,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+    ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+    ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+  },
+}
